b175359d2aa3c9cd1ed5078b5685bf368f719f20
Ojārs <ojars.meldris@gmail.com>
2022-11-16 10:48:54 +0200

    update

--- app/CompaniesRegister.php
+++ app/CompaniesRegister.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use League\Csv\Reader;
-use League\Csv\Statement;
 
 class CompaniesRegister
 {
@@ -15,44 +14,39 @@ class CompaniesRegister
         $csv->setDelimiter($delimiter);
         $csv->setHeaderOffset(0);
         foreach ($csv as $record) {
-            $this->companies[]= new Company($record['name'],$record['regcode']);
+            $this->companies[] = new Company($record['name'], $record['regcode']);
         }
-    }
-
-    public function getCompanies(): array
-    {
-        return $this->companies;
     }
 
     public function getCompaniesRegister(): string
     {
-        $companiesRegister='NOSAUKUMS'.str_repeat(' ',41).'- REĢISTRĀCIJAS NUMURS'.PHP_EOL;
-        for ($i=count($this->companies)-30; $i<count($this->companies); $i++){
-            $selectedCompany=$this->companies[$i];
-            $count=strlen($selectedCompany->getName());
-            $space=str_repeat(' ',50-$count).'- ';
-            $companiesRegister=$companiesRegister.$selectedCompany->getName().$space.$selectedCompany->getRegistrationCode().PHP_EOL;
+        $companiesRegister = 'NOSAUKUMS' . str_repeat(' ', 41) . '- REĢISTRĀCIJAS NUMURS' . PHP_EOL;
+        for ($i = count($this->companies) - 30; $i < count($this->companies); $i++) {
+            $selectedCompany = $this->companies[$i];
+            $count = strlen($selectedCompany->getName());
+            $space = str_repeat(' ', 50 - $count) . '- ';
+            $companiesRegister = $companiesRegister . $selectedCompany->getName() . $space . $selectedCompany->getRegistrationCode() . PHP_EOL;
         }
         return $companiesRegister;
     }
 
     public function searchByName(string $name): string
     {
-        foreach ($this->companies as $company){
-            if ($company->getName()==$name){
-                return PHP_EOL."Uzņēmums '$name' tika atrasts, tā reģistrācijas numurs ir {$company->getRegistrationCode()}.";
+        foreach ($this->companies as $company) {
+            if ($company->getName() == $name) {
+                return PHP_EOL . "Uzņēmums '$name' tika atrasts, tā reģistrācijas numurs ir {$company->getRegistrationCode()}.";
             }
         }
-        return PHP_EOL."Uzņēmums '$name' netika atrasts!";
+        return PHP_EOL . "Uzņēmums '$name' netika atrasts!";
     }
 
     public function searchByRegistrationCode(string $registrationCode): string
     {
-        foreach ($this->companies as $company){
-            if ($company->getRegistrationCode()==$registrationCode){
-                return PHP_EOL."Uzņēmums ar reģistrācijas numuru: $registrationCode tika atrasts, tās nosaukums ir '{$company->getName()}'.";
+        foreach ($this->companies as $company) {
+            if ($company->getRegistrationCode() == $registrationCode) {
+                return PHP_EOL . "Uzņēmums ar reģistrācijas numuru: $registrationCode tika atrasts, tās nosaukums ir '{$company->getName()}'.";
             }
         }
-        return PHP_EOL."Uzņēmums ar reģistrācijas numuru $registrationCode netika atrasts!";
+        return PHP_EOL . "Uzņēmums ar reģistrācijas numuru $registrationCode netika atrasts!";
     }
 }
--- index.php
+++ index.php
@@ -4,31 +4,31 @@ require_once 'vendor/autoload.php';
 
 use App\CompaniesRegister;
 
-$companies=new CompaniesRegister('register.csv',';');
+$companies = new CompaniesRegister('register.csv', ';');
 while (true) {
-    while (true){
-        echo "[1] Apskatīt pēdējos 30 ierakstus uzņēmuma reģistrā.".PHP_EOL;
-        echo "[2] Meklēt uzņēmuma reģistrācijas numuru pēc uzņēmuma nosaukuma.".PHP_EOL;
-        echo "[3] Meklēt uzņēmuma nosaukumu pēc reģistrācijas numura.".PHP_EOL;
-        echo "[4] Iziet no aplikācijas.".PHP_EOL;
+    while (true) {
+        echo "[1] Apskatīt pēdējos 30 ierakstus uzņēmuma reģistrā." . PHP_EOL;
+        echo "[2] Meklēt uzņēmuma reģistrācijas numuru pēc uzņēmuma nosaukuma." . PHP_EOL;
+        echo "[3] Meklēt uzņēmuma nosaukumu pēc reģistrācijas numura." . PHP_EOL;
+        echo "[4] Iziet no aplikācijas." . PHP_EOL;
         $choice = readline('Izvēlies numuru darbībai ko vēlies veikt: ');
-        switch ($choice){
+        switch ($choice) {
             case 1:
-                echo PHP_EOL."UZŅĒMUMU REĢISTRS PĒDĒJIE 30 IERAKSTI:".PHP_EOL;
+                echo PHP_EOL . "UZŅĒMUMU REĢISTRS PĒDĒJIE 30 IERAKSTI:" . PHP_EOL;
                 echo $companies->getCompaniesRegister();
                 break;
             case 2:
-                $name=readline('Ievadi uzņēmuma nosaukumu: ');
-                echo $companies->searchByName($name).PHP_EOL;
+                $name = readline('Ievadi uzņēmuma nosaukumu: ');
+                echo $companies->searchByName($name) . PHP_EOL;
                 break;
             case 3:
-                $registrationCode=readline('Ievadi uzņēmuma reģistrācijas numuru: ');
-                echo $companies->searchByRegistrationCode($registrationCode).PHP_EOL;
+                $registrationCode = readline('Ievadi uzņēmuma reģistrācijas numuru: ');
+                echo $companies->searchByRegistrationCode($registrationCode) . PHP_EOL;
                 break;
             case 4:
-                exit('Uz redzēšanos!'.PHP_EOL);
+                exit('Uz redzēšanos!' . PHP_EOL);
             default:
-                echo PHP_EOL."Nekorekta ievade!";
+                echo PHP_EOL . "Nekorekta ievade!";
         }
         echo PHP_EOL;
     }
